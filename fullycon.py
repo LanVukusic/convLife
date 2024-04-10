ca7fca0e59c306c74d52679b104311652d2d3585
gendestry <zan.oberstar@gmail.com>
2024-04-11 00:07:50 +0200

    mja malo skifo

--- fullycon.py
+++ fullycon.py
@@ -7,64 +7,52 @@ class CullyConnCoder (torch.nn.Module):
         self.size = int(field_size)
         # self.nekaj = int(self.size/2)
 
-        self.conv1 = torch.nn.Conv2d(1, 8, 3, padding=1, )
-        self.conv2 = torch.nn.Conv2d(8, FILTERS, 3, padding=1)
-       
-        self.linear = torch.nn.Sequential(
-            torch.nn.Linear(FILTERS*FILTERS*FILTERS, 128),
-            torch.nn.ReLU(inplace=True),
-
-            torch.nn.Linear(128, 128),
-            # torch.nn.Dropout(0.15),
-            torch.nn.ReLU(inplace=True),
-
-            # torch.nn.Linear(128, 128),
-            # torch.nn.Dropout(0.3),
-            # torch.nn.ReLU(inplace=True),
-
-            # torch.nn.Linear(128, 128),
-            # torch.nn.Dropout(0.15),
-            # torch.nn.ReLU(inplace=True),
-
-            torch.nn.Linear(128, self.size*self.size),
-            torch.nn.LogSigmoid(),
-        )
-
-        # self.linear2 = torch.nn.Sequential(
-        #     torch.nn.Linear(self.size*self.size, 64),
-        #     torch.nn.ReLU(inplace=True),
-
-        #     torch.nn.Linear(64, self.size*self.size),
-        #     torch.nn.Sigmoid(),
-        # )
-
-        # self.end_conv = torch.nn.Sequential(
-        #     torch.nn.Conv2d(1, 1, 3, padding=1, stride=1),
-        #     torch.nn.ReLU(),
-        #     torch.nn.Conv2d(1, 1, 3, padding=1, stride=1),
-        #     torch.nn.ReLU(),
-        # )
-
-
-
-        # self.l1 = torch.nn.Linear(self.size * self.size, 50)
-        # self.l2 = torch.nn.Linear(50, 50)
-        # self.l3 = torch.nn.Linear(50, self.size*self.size)
-
+        self.conv1 = torch.nn.Conv2d(1, 1, 3, padding=1, bias=True)
+        self.conv2 = torch.nn.Conv2d(1, 1, 3, padding=1, bias=True)
+        # self.batch1 = torch.nn.BatchNorm2d(8)
+        # self.conv2 = torch.nn.Conv2d(1, 1, 3, padding=1, bias=True)
+        # self.batch2 = torch.nn.BatchNorm2d(1)
+        # self.conv3 = torch.nn.Conv2d(8, 8, 3, padding=1,)
+        # self.conv4 = torch.nn.Conv2d(8, 8, 3, padding=1,)
+        # self.conv5= torch.nn.Conv2d(8, 1, 3, padding=1,)
+        
     def forward(self, x:torch.Tensor):
         x = x.float()
         out = x
         out = self.conv1(out)
         out = torch.nn.functional.relu(out)
-
         out = self.conv2(out)
         out = torch.nn.functional.relu(out)
-        # print("convolved", x.shape)
+        # out = self.batch1(out)
+        # out = torch.nn.functional.relu(out)
+        # out = self.conv2(out)
+        # out = self.batch2(out)
+        # out = torch.nn.functional.relu(out)
+        # out = self.conv1(out)
+        # out = self.batch1(out)
+        # out = torch.nn.functional.relu(out)
+        # out = self.conv2(out)
+        # out = self.batch2(out)
+        # out = torch.nn.functional.relu(out)
+        # out = out + out1
+        # out = self.conv3(out)
+        # out2 = torch.nn.functional.relu(out)
+        # out = self.conv4(out2)
+        # out = torch.nn.functional.relu(out)
+        # out = out + out2
+        # out = self.conv5(out)
+        # out = torch.nn.functional.relu(out)
         
-        out = out.view(-1, FILTERS*self.size*self.size)
-        # print("input", x.shape)
-        out = self.linear(out)
-        out = out.view(-1, 1, self.size, self.size)
+        # out = torch.nn.functional.relu(out)
+
+        # out = self.conv2(out)
+        # out = torch.nn.functional.relu(out)
+        # # print("convolved", x.shape)
+        
+        # out = out.view(-1, FILTERS*self.size*self.size)
+        # # print("input", x.shape)
+        # out = self.linear(out)
+        # out = out.view(-1, 1, self.size, self.size)
 
         # skip conn
         # out = torch.concat(out, x).clamp(0, 1)
